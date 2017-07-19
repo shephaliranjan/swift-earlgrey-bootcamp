c90747e9622fc1499954f6c5e541c375fab249de
KaHin Ng and Nicholas Barron <pair+kang+nbarron@pivotal.io>
2017-07-19 16:28:47 -0400

    demo test

--- swift-earlgrey-bootcampTests/swift_earlgrey_bootcampTests.swift
+++ swift-earlgrey-bootcampTests/swift_earlgrey_bootcampTests.swift
@@ -1,36 +1,47 @@
-//
-//  swift_earlgrey_bootcampTests.swift
-//  swift-earlgrey-bootcampTests
-//
-//  Created by Pivotal on 2017-05-08.
-//
-//
-
 import XCTest
+import EarlGrey
+
 @testable import swift_earlgrey_bootcamp
 
 class swift_earlgrey_bootcampTests: XCTestCase {
     
+    let storyboard = UIStoryboard(name: "Main",
+                                  bundle: Bundle.main)
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testStep0() {
+        EarlGrey.select(elementWithMatcher: grey_allOf([grey_accessibilityTrait(UIAccessibilityTraitHeader), grey_accessibilityLabel("Home")]))
+        .assert(grey_sufficientlyVisible())
+        for number in 1...4 {
+            EarlGrey.select(elementWithMatcher: grey_allOf([grey_accessibilityTrait(UIAccessibilityTraitStaticText), grey_accessibilityLabel("Step \(number)")]))
+                .assert(grey_sufficientlyVisible())
         }
     }
     
+    func testStep1() {
+        let presentViewController = storyboard.instantiateViewController(withIdentifier: "Step1")
+        let navigationController = UIApplication.shared.keyWindow?.rootViewController as! UINavigationController
+        navigationController.pushViewController(presentViewController, animated: false)
+        
+        EarlGrey.select(elementWithMatcher: grey_allOf([grey_accessibilityTrait(UIAccessibilityTraitButton), grey_accessibilityLabel("Home")]))
+            .assert(grey_sufficientlyVisible())
+        
+        EarlGrey.select(elementWithMatcher: grey_allOf([grey_accessibilityLabel("Check This"), grey_accessibilityTrait(UIAccessibilityTraitStaticText), grey_not(grey_anyOf([grey_kindOfClass(NSClassFromString("UIButtonLabel")!)])), grey_ancestor(grey_allOf([grey_kindOfClass(UIStackView.self), grey_descendant(grey_kindOfClass(UIButton.self))]))]))
+            .assert(grey_sufficientlyVisible())
+        EarlGrey.select(elementWithMatcher: grey_allOf([grey_accessibilityLabel("Check This"), grey_accessibilityTrait(UIAccessibilityTraitButton)]))
+            .assert(grey_sufficientlyVisible())
+        
+        EarlGrey.select(elementWithMatcher: grey_allOf([grey_accessibilityLabel("Check This"), grey_accessibilityTrait(UIAccessibilityTraitStaticText), grey_not(grey_anyOf([grey_kindOfClass(NSClassFromString("UITextFieldLabel")!)])), grey_ancestor(grey_allOf([grey_kindOfClass(UIStackView.self), grey_descendant(grey_kindOfClass(UITextField.self))]))]))
+            .assert(grey_sufficientlyVisible())
+        EarlGrey.select(elementWithMatcher: grey_accessibilityValue("Check This"))
+            .assert(grey_sufficientlyVisible())
+        
+    }
 }
